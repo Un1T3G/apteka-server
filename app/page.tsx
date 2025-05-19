b1b3ad59d9cb15c80e416ed1831f5c1a384e5019
Un1T3G <un1t3gx@gmail.com>
2025-05-19 14:51:06 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { NoSSR } from "@/lib/no-ssr";
 export const revalidate = 0;
 
 async function getCategories() {
-  const response = await fetch(`http://localhost:3000/api/category`);
+  const response = await fetch(`${process.env.CLIENT_URL}/api/category`);
   return await response.json();
 }
 
